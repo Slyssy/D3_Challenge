a25a4c97c31f5a2f126cdd217b9131a2ff0acf1c
Stephen <stephen.lyssy@gmail.com>
2020-12-13 21:53:04 -0600

    Built scatter plot that shows the relationship between poverty and smoking in every state in the US. Still need to address placing it in the HTML.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,93 @@
-// @TODO: YOUR CODE HERE!
+// Set SVG Area
+const svgHeight = 600
+const svgWidth = 800
+
+// Set Margins
+const margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  }
+
+// Defining Chart area inside of SVG
+const chartHeight = svgHeight - margin.top - margin.bottom
+const chartWidth = svgWidth - margin.left - margin.right
+
+// Setting Bubble Size Limits
+const minRadius = 16
+const maxRadius = 16
+
+const svg = d3.select("body").append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth)
+
+  const chartG = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+// Load CSV Data
+  d3.csv("assets/data/data.csv").then(data => {
+    console.log(data)
+
+// Begin Set Scales
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data.map(d => parseFloat(d.smokes)))])
+        // invert axis to avoid having to do d3 axis calculations
+        .range([chartHeight, 0])
+
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data.map(d => parseFloat(d.poverty)))])
+        .range([0, chartWidth])
+    
+    const size = d3.scaleLinear()
+        .domain(d3.extent(data.map(d => parseInt(d.income))))
+        .range([minRadius, maxRadius])
+
+// Begin Define Axis
+    const yAxis = d3.axisLeft(y)
+    const xAxis = d3.axisBottom(x)
+
+    chartG.append("g")
+        .call(yAxis)
+    
+    // const labelAreaY = svg
+    // .append("g")
+    // .attr(
+    //     "transform",
+    //     `translate(${svgHeight / 2}, ${svgWidth - margin.left + 20})`
+    // );
+
+    // labelAreaY.append("text").attr("stroke", "#000000").text("(%)Smokers");
+
+    chartG.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(xAxis)
+
+    const labelAreaX = svg
+        .append("g")
+        .attr(
+          "transform",
+          `translate(${svgWidth / 2}, ${svgHeight - margin.bottom + 45})`
+        );
+    
+    labelAreaX.append("text").attr("stroke", "#000000").text("In Poverty (%)");  
+
+    const plotArea = chartG.append("g")
+    const circleG = plotArea.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", d => `translate(${x(parseFloat(d.poverty))}, ${y(parseFloat(d.smokes))})`)
+            .attr("fill", "#62bbd1")
+            // .attr("r", 16)
+    
+    circleG.append("circle")
+            .attr("r", d => size(parseInt(d.income)))
+
+    circleG.append("text")
+            .text(d => d.abbr)
+            .attr("stroke", "#FFFFFF")
+            .attr("fill", "#FFFFFF")
+            .attr("dy", ".3em")
+            .attr("text-anchor", "middle")    
+  })
