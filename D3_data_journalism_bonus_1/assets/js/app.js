c6e7d8e28d0c02fcf3444738d58d468f59404c76
Stephen <stephen.lyssy@gmail.com>
2020-12-18 14:02:40 -0600

    Cleaned up and deleted unused code.

--- D3_data_journalism_bonus_1/assets/js/app.js
+++ D3_data_journalism_bonus_1/assets/js/app.js
@@ -1,112 +1,3 @@
-// // Set SVG Area
-// const svgHeight = 600;
-// const svgWidth = 800;
-
-// // Set Margins
-// const margin = {
-//   top: 50,
-//   right: 50,
-//   bottom: 100,
-//   left: 100,
-// };
-
-// // Defining Chart area inside of SVG
-// const chartHeight = svgHeight - margin.top - margin.bottom;
-// const chartWidth = svgWidth - margin.left - margin.right;
-
-// // Setting Bubble Size Limits
-// // const minRadius = 12;
-// // const maxRadius = 20;
-
-// const svg = d3
-//   .select("#scatter")
-//   .append("svg")
-//   .attr("height", svgHeight)
-//   .attr("width", svgWidth);
-
-// const chartG = svg
-//   .append("g")
-//   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// // Load CSV Data
-// d3.csv("assets/data/data.csv").then((data) => {
-//   console.log(data);
-
-//   // Begin Set Scales
-//   const y = d3
-//     .scaleLinear()
-//     .domain([6, d3.max(data.map((d) => parseFloat(d.smokes)))])
-//     // invert axis to avoid having to do d3 axis calculations
-//     .range([chartHeight, 0]);
-
-//   const x = d3
-//     .scaleLinear()
-//     .domain([8, d3.max(data.map((d) => parseFloat(d.poverty)))])
-//     .range([0, chartWidth]);
-
-//   // const size = d3
-//   //   .scaleLinear()
-//   //   .domain(d3.extent(data.map((d) => parseInt(d.income))))
-//   //   .range([minRadius, maxRadius]);
-
-//   // Begin Define Axis
-//   const yAxis = d3.axisLeft(y);
-//   const xAxis = d3.axisBottom(x);
-
-//   // Setting up y axis label
-//   chartG.append("g").call(yAxis);
-
-//   const labelAreaY = svg
-//     .append("g")
-//     .attr(
-//       "transform",
-//       `translate( ${svgWidth - margin.left - 645}, ${svgHeight / 2})`
-//     );
-
-//   labelAreaY
-//     .append("text")
-//     .attr("stroke", "#000000")
-//     .attr("transform", "rotate(-90)")
-//     .text("(%) Smokers");
-
-//   // Setting up x axis label
-//   chartG
-//     .append("g")
-//     .attr("transform", `translate(0, ${chartHeight})`)
-//     .call(xAxis);
-
-//   const labelAreaX = svg
-//     .append("g")
-//     .attr(
-//       "transform",
-//       `translate(${svgWidth / 2}, ${svgHeight - margin.bottom + 45})`
-//     );
-
-//   labelAreaX.append("text").attr("stroke", "#000000").text("In Poverty (%)");
-
-//   // const plotArea = chartG.append("g");
-//   const circleG = chartG.selectAll("circle")
-//     // .selectAll("g")
-//     .data(data)
-//     .enter()
-//     .append("circle")
-//     .attr("cx", d =>x(parseFloat(d.poverty)))
-//     .attr("cy", d =>y(parseFloat(d.smokes)))
-//     .attr("fill", "#62bbd1")
-//     .attr("r", 12);
-
-//   // circleG.append("circle").attr("r", (d) => size(parseInt(d.income)));
-
-//   circleG.selectAll
-//     .append("text")
-//     .text((d) => d.abbr)
-//     .attr("stroke", "#FFFFFF")
-//     .attr("fill", "#FFFFFF")
-//     .attr("dy", ".2em")
-//     .attr("text-anchor", "middle");
-// });
-
-// With Labels Begins Here
 // Set SVG Area
 const svgHeight = 600;
 const svgWidth = 800;
@@ -122,10 +13,6 @@ const margin = {
 // Defining Chart area inside of SVG
 const chartHeight = svgHeight - margin.top - margin.bottom;
 const chartWidth = svgWidth - margin.left - margin.right;
-
-// // Setting Bubble Size Limits
-// const minRadius = 12;
-// const maxRadius = 20;
 
 const svg = d3
   .select("#scatter")
@@ -224,20 +111,12 @@ d3.csv("assets/data/data.csv").then((data) => {
   const y = d3
     .scaleLinear()
     .domain([6, d3.max(data.map((d) => parseFloat(d.smokes)))])
-    // invert axis to avoid having to do d3 axis calculations
     .range([chartHeight, 0]);
 
   const x = d3
     .scaleLinear()
     .domain([8, d3.max(data.map((d) => parseFloat(d.poverty)))])
-    .range([0, chartWidth]);
-
-   
-
-  // const size = d3
-  //   .scaleLinear()
-  //   .domain(d3.extent(data.map((d) => parseInt(d.income))))
-  //   .range([minRadius, maxRadius]);
+    .range([0, chartWidth]); 
 
   // Begin Define Axis
   const yAxis = d3.axisLeft(y);
@@ -330,9 +209,6 @@ d3.csv("assets/data/data.csv").then((data) => {
     .attr("opacity", "0.75")
     .attr("r", 12);
 
-  // circleG.append("circle").attr("r", (d) => size(parseInt(d.income)));
-  
-
   circle_text = circleG
     .append("text")
     .text((d) => d.abbr)
@@ -343,4 +219,4 @@ d3.csv("assets/data/data.csv").then((data) => {
     .attr("text-anchor", "middle");
     });
 
-    
+  
